749e2e5538027ecc38bc3cca750fee91873a437b
huihui.zhang <glorydom@163.com>
2018-04-21 01:05:05 +0800

    模板功能完成

--- index_135editor.js
+++ index_135editor.js
@@ -1,4 +1,5 @@
 $(function () {
+    var templateCache = {};
     var filterUrl = 'http://www.135editor.com/editor_styles/styles?inajax=1&page=1&free=0&fav=fav&sort=favor:desc&filter=';
     $("[data-filter]").click(function () {
         // alert($(this).attr('data-filter'));
@@ -8,16 +9,20 @@ $(function () {
         });
     });
 
+    //点击系统模板，将模板放在左侧样式里
     $("[href='#systemTemplates']").click(function () {
 
         if (!$.trim($("#systemTemplates").html())) {
             var url = 'http://www.135editor.com/editor_styles/systemTemplates';
             $.get(url).then(function (data) {
-                // console.log($(data).find("#system-template-list").html());
+                templateCache = cacheTemplatesInfo(data); //统计所有的template的相关信息
                 var templateHtml = $(data).find("#system-template-list").html();
                 $("#systemTemplates").html(templateHtml);
             });
         }
+
+        $("#systemTemplates").on("mouseenter", ".style-list", popupTemplateOptions);
+        $("#systemTemplates").on("mouseleave", ".style-list", hideTemplateOptions);
     });
 
     // $(".style-result").on("click", "li", function () {
@@ -27,10 +32,10 @@ $(function () {
     // });
     $(".style-result").on("click", "li", applyStyle);
 
-});
 
 
- //Ueditor编辑器里面选中内容之后，点击左侧样式，该样式将被应用在选中的内容上
+
+//Ueditor编辑器里面选中内容之后，点击左侧样式，该样式将被应用在选中的内容上
 function applyStyle() {
     var ue = UE.getEditor('editor');
     var range = ue.selection.getRange();
@@ -42,5 +47,81 @@ function applyStyle() {
 
 }
 
+//在模板上弹出 秒刷 插入 按钮
+function popupTemplateOptions() {
+    var templateId = $(this).attr("id").split("-")[2];
+    var url = "http://www.135editor.com" + templateCache[templateId];
 
+    var cover = $("<div></div>").css("background-color", "gray")
+        .css("position", "absolute")
+        .css("top", "70px")
+        .css("z-index", 1)
+        .addClass("cover");
+    var miaoshua = $("<button>秒刷</button>")
+        .css("margin-right", "10px")
+        .on("click", function () {
+            var templateContainer = $("<div></div>")
+                .addClass("tab-pane active")
+                .css("background-color", "gray")
+                .css("position", "absolute")
+                .css("top", 0)
+                .css("left", 0)
+                .css("z-index", 2);
+            var closeTitle = $("<p>样式模板</p>");
+            var closeContainer = $("<button></button>").text("关闭")
+                .on("click", function () {
+                    templateContainer.remove();
+
+                });
+            templateContainer.append(closeTitle);
+            templateContainer.append(closeContainer);
+            $.get(url).then(function (data) {
+                // console.log($(data).find("#system-template-list").html());
+                var templateHtml = $(data).find(".Content-body").html();
+                templateContainer.append($(templateHtml));
+            });
+
+            $('#systemTemplates').append(templateContainer);
+
+        });
+    var charu = $("<button></button>")
+        .text("插入")
+        .on("click", function () {
+            var ue = UE.getEditor('editor');
+            var range = ue.selection.getRange();
+            range.select();
+            $.get(url).then(function (data) {
+                // console.log($(data).find("#system-template-list").html());
+                var templateHtml = $(data).find(".Content-body").html();
+                ue.execCommand("inserthtml", templateHtml);
+            });
+        });
+
+    cover.append(miaoshua);
+    cover.append(charu);
+    $(this).append(cover);
+}
+
+function hideTemplateOptions() {
+    $(this).find(".cover").remove();
+}
+
+
+
+
+//统计模板的id与该模板的url关联关系
+// eg. <div class="style-list clearfix" id="editor-style-91571">
+// ......
+//  <a href="/editor_styles/20180103/91571.html" target="_blank">电影推送 模板</a>
+function cacheTemplatesInfo(html) {
+    var cache = {};
+    $(html).find(".style-list").each(function () {
+        var id = $(this).attr("id");
+        id = id.split("-")[2];
+        cache[id] = $(this).find("a[target='_blank']").attr("href");
+    });
+    return cache;
+}
+
+});
 
